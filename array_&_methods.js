33122dd17fbd5e2a6c6862f7569b9c5a0ef568df
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2025-06-28 00:28:03 +0600

    Apna College

---
+++ array_&_methods.js
@@ -0,0 +1,7 @@
+let companies = ["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+console.log(companies.shift()); //to dlt & return the first ele
+console.log(companies); //after dlt
+console.log(companies.splice(1,1,"Ola"));//starting index,starting indx er pore koita dlt,dlt index e ki dibo
+console.log(companies);
+companies.push("Amazon"); //to add at end
+console.log(companies);
