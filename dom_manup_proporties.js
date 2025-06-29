a92579ca28bfd4a7aafeb14a46dfa18e35ed3308
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2025-06-30 01:43:49 +0600

    02

---
+++ dom_manup_proporties.js
@@ -0,0 +1,7 @@
+//alert("Welcome to my page");
+let x = document.getElementById("t1");
+let y = document.getElementById("t2");
+console.dir(x);
+console.dir(y);
+let class_eles = document.getElementsByClassName("topic");
+console.dir(class_eles);
